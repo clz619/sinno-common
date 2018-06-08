d7ef345763ab653342ae316f2f13158eec4d8e19
lizhong.chen <admin@chenlizhong.cn>
2018-06-08 14:35:25 +0800

    [ADD] add pinyin

---
+++ src/main/java/win/sinno/common/util/PinyinUtil.java
@@ -0,0 +1,77 @@
+package win.sinno.common.util;
+
+import net.sourceforge.pinyin4j.PinyinHelper;
+import net.sourceforge.pinyin4j.format.HanyuPinyinCaseType;
+import net.sourceforge.pinyin4j.format.HanyuPinyinOutputFormat;
+import net.sourceforge.pinyin4j.format.HanyuPinyinToneType;
+import net.sourceforge.pinyin4j.format.HanyuPinyinVCharType;
+import net.sourceforge.pinyin4j.format.exception.BadHanyuPinyinOutputFormatCombination;
+
+/**
+ * win.sinno.common.util.PinyinUtil
+ *
+ * @author chenlizhong@qipeng.com
+ * @date 2018/6/8
+ */
+public class PinyinUtil {
+
+  private static final HanyuPinyinOutputFormat hanyuPinyinOutputFormat = new HanyuPinyinOutputFormat();
+
+  static {
+    hanyuPinyinOutputFormat.setCaseType(HanyuPinyinCaseType.LOWERCASE);
+    hanyuPinyinOutputFormat.setToneType(HanyuPinyinToneType.WITHOUT_TONE);
+    hanyuPinyinOutputFormat.setVCharType(HanyuPinyinVCharType.WITH_V);
+  }
+
+  public static String format2PinYin(String chinese) {
+
+    StringBuilder output = new StringBuilder();
+    char[] chars = chinese
+        .replaceAll("【", "")
+        .replaceAll("】", "")
+        .trim()
+        .toCharArray();
+    for (int i = 0; i < chars.length; i++) {
+      try {
+        if (chars[i] > 128) {
+          String[] temp = PinyinHelper.toHanyuPinyinStringArray(chars[i], hanyuPinyinOutputFormat);
+          for (String s : temp) {
+            output.append(s);
+          }
+
+        } else {
+          output.append(chars[i]);
+        }
+      } catch (BadHanyuPinyinOutputFormatCombination badHanyuPinyinOutputFormatCombination) {
+        badHanyuPinyinOutputFormatCombination.printStackTrace();
+      }
+    }
+
+    return output.toString();
+  }
+
+  public static String format2PinYinFirstChar(String chinese) {
+
+    StringBuilder output = new StringBuilder();
+    char[] chars = chinese
+        .replaceAll("【", "")
+        .replaceAll("】", "")
+        .trim()
+        .toCharArray();
+    for (int i = 0; i < chars.length; i++) {
+      try {
+        if (chars[i] > 128) {
+          String[] temp = PinyinHelper.toHanyuPinyinStringArray(chars[i], hanyuPinyinOutputFormat);
+
+          output.append(temp[0].charAt(0));
+        } else {
+          output.append(chars[i]);
+        }
+      } catch (BadHanyuPinyinOutputFormatCombination badHanyuPinyinOutputFormatCombination) {
+        badHanyuPinyinOutputFormatCombination.printStackTrace();
+      }
+    }
+
+    return output.toString();
+  }
+}
---
+++ src/test/java/win/sinno/common/util/PinyinUtilTest.java
@@ -0,0 +1,21 @@
+package win.sinno.common.util;
+
+import org.junit.Test;
+
+/**
+ * win.sinno.common.util.PinyinUtilTest
+ *
+ * @author chenlizhong@qipeng.com
+ * @date 2018/6/8
+ */
+public class PinyinUtilTest {
+
+  @Test
+  public void testPinyin() {
+
+    String chinese = "abc铭佳童话123旗舰店ert";
+
+    System.out.println(PinyinUtil.format2PinYin(chinese));
+    System.out.println(PinyinUtil.format2PinYinFirstChar(chinese));
+  }
+}
